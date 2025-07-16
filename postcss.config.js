78b3753ea5924b288e3f005fce231a34a8540859
schollaertel <erinchantz@yahoo.com>
2025-07-16 19:56:00 -0400

    stuip
    commit

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,7 @@
-// postcss.config.js
 export default {
   plugins: {
-    '@tailwindcss/postcss': {},   // ← the new package!
-    autoprefixer: {},             // you can keep this if you like
+    '@tailwindcss/postcss': {},
+    autoprefixer: {},
   },
 }
 
