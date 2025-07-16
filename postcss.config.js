653a0aa247edfa72afe89b65b4dea55094ca4a66
schollaertel <erinchantz@yahoo.com>
2025-07-16 19:12:22 -0400

    Ready for deploy

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+// postcss.config.js
+export default {
+  plugins: {
+    '@tailwindcss/postcss': {},   // ← the new package!
+    autoprefixer: {},             // you can keep this if you like
+  },
+}
+
+  
